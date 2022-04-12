fe998dd21b5623de8a3eb2fbb7b9f29db473b1f7
vlad <vlad_kuznetsov_01@internet.ru>
2022-04-12 12:21:18 +0300

    add changings Добавьте файлы проекта.Добавьте файлы проекта.

--- GitHub_pro/GitHub_pro.cpp
+++ GitHub_pro/GitHub_pro.cpp
@@ -8,6 +8,11 @@ int main()
 	using namespace boost::lambda;
 	typedef std::istream_iterator<int> in;
 
+	std::cout << "change" << std::endl;
+	std::cout << "change 1" << std::endl;
+	std::cout << "change 2" << std::endl;
+	std::cout << "change 3" << std::endl;
+
 	std::for_each(
 		in(std::cin), in(), std::cout << (1 * 3) << "");
 }
